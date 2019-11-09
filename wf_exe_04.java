270db2c094410dabbbc5d3e73c0982308ccb1b92
gw <55128506+1971550874@users.noreply.github.com>
2019-11-09 09:53:08 +0800

    功能3
    java wf_exe_04 -d-s <file> -n 个数

---
+++ wf_exe_04.java
@@ -0,0 +1,156 @@
+import java.io.*;
+import java.text.DecimalFormat;
+import java.util.*;
+
+/**
+ * 功能三：
+ */
+public class wf_exe_04 {
+
+    static int number = 0;//记录总单词的个数
+
+    public static void main(String[] args) throws IOException {
+        if ("-d-s".equals(args[0]) && "-n".equals(args[2])){
+            if (args[3] != null)
+                readMkdir(args[1], Integer.parseInt(args[3]));
+            else readMkdir(args[1],-1);
+        }else{
+            System.out.println("请输入正确的参数");
+        }
+    }
+
+    //遍历文件读取单词放入list数组
+    public static void readFile_word(String s, HashMap<String,Integer> hashMap) throws IOException {
+
+        FileReader reader = new FileReader(s);
+        //缓存数组
+        BufferedReader bf = new BufferedReader(reader);
+        List<String> list = new ArrayList<String>();
+        String line;
+        while ((line = bf.readLine()) != null) {
+            list.add(line);
+        }
+        reader.close();
+        String receiveString = join(list, "");//数组拼接成字符串
+
+        //将字符串按照空格切割
+        String[] word = receiveString.split(" ");
+        int i = 0;
+
+        //去除单词中参杂的其他的东西
+        while (i < word.length) {
+            if (!word[i].matches("^[a-zA-Z]{0,}$")) {
+                char[] chars = word[i].toCharArray();
+                int j = 0;
+                for (j = 0; j < chars.length; j++) {
+                    if (!(chars[j] >= 'a' && chars[j] <= 'z' || chars[j] >= 'A' && chars[j] <= 'Z')) {
+                        String[] str = word[i].split(String.valueOf(chars[j]));
+                        StringBuffer sb = new StringBuffer();
+                        for (int k = 0; k < str.length; k++) {
+                            sb.append(str[k]);
+                        }
+                        word[i] = sb.toString();
+                    }
+                }
+            }
+            i++;
+        }
+
+        number += word.length;//记录总单词出现的总个数
+
+       Set<String> wordSet = hashMap.keySet();
+
+        for (int x = 0; x < word.length; x++) {
+            if (wordSet.contains(word[x])) {
+                Integer number = hashMap.get(word[x]);
+                hashMap.replace(word[x],number, ++number);
+            } else {
+                hashMap.put(word[x], 1);
+            }
+        }
+    }
+    //将list数组转化为字符串
+    public static String join(Collection var0, String var1) {
+        StringBuffer var2 = new StringBuffer();
+        for(Iterator var3 = var0.iterator(); var3.hasNext(); var2.append((String)var3.next())) {
+            if (var2.length() != 0) {
+                var2.append(var1);
+            }
+        }
+
+        return var2.toString();
+    }
+
+    //-d-s
+    public static void readMkdir(String s,int n) throws IOException {
+
+        HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
+        List<String> list = new ArrayList<String>();
+        getAllFileName(s , list);
+        for (String str : list) {
+            readFile_word(str , hashMap);
+        }
+        int sum = 0;
+
+        for (String k:hashMap.keySet())
+            sum++;
+        if (n >= 0 && n < sum)
+            sum = n;
+        while (sum-- > 0) {
+            Iterator<String> iterator = hashMap.keySet().iterator();
+            int max = 0;
+            String maxword = null;
+            while (iterator.hasNext()) {
+                String wd = iterator.next();
+                if (hashMap.get(wd) > max) {
+                    max = hashMap.get(wd);
+                    maxword = wd;
+                }
+            }
+            hashMap.remove(maxword);
+            double ans=max*1.0/ number;
+            DecimalFormat df = new DecimalFormat("0.00%");
+            String r = df.format(ans);
+            if (!"".equals(maxword)) {
+                System.out.format("%-20s : %s\n", maxword, r);
+                //n--;
+            }
+        }
+    }
+
+    //获取文件夹下的文件
+    public static File[] getCurFilesList(String filePath) {
+            File mkdir = new File(filePath);
+                File[] files = mkdir.listFiles(new FileFilter() {
+                    @Override
+                    public boolean accept(File pathname) {
+                        return ! pathname.isDirectory();
+                    }
+                });
+                return files;
+    }
+
+    //递归文件夹找到所有的文件
+    public static void getAllFileName(String path, List<String> fileName)
+    {
+        File file = new File(path);
+        File [] files = file.listFiles();
+
+        if(file.isDirectory()){
+            File[] filesList = getCurFilesList(path);
+            for (File a : filesList) {
+                    fileName.add(a.getAbsolutePath());
+            }
+
+            for(File a:files)
+            {
+                if(a.isDirectory())
+                {
+                    getAllFileName(a.getAbsolutePath(), fileName);
+                }
+            }
+        }else {
+            System.out.println("请输入文件夹！！！");
+        }
+    }
+}
